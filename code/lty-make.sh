46963a7b2735f3fce688ba6893d4f72cb18f2d67
天佑 刘 <1347277058@qq.com>
2020-08-15 15:24:08 +0800

    zhouwc update

--- code/lty-make.sh
+++ code/lty-make.sh
@@ -34,6 +34,84 @@
 # So, don't press any keys wrong!
 # And FUCK you, "dear" BUGS!
 
+#                                      Legends never die!
+#                                                               ...                                                                  
+#                                                              .oocc.                                                                
+#                                                             .c...ccc.                                                              
+#                                                            .cc  ....coc                                                            
+#                                                           .c.   ......co.                                                          
+#                                                           .c.    .......oc.                                                        
+#                                                           .c.   . ........co.                                                       
+#                                                          cc   .  .......ccccc                                                      
+#                                                          cc.     ......ccc.ccc.                                                    
+#                                          ....           .cc....   ....ccccc.ccc.                                                   
+#                                         .occczzc..      c....c...  ..ccccccc.ccc.                                                  
+#                                         .cc....cozzc.  .o.....cc.....cccccccc..cu.                                                 
+#                                         o........couuuzuccc.....c.cccccccccccc.cc.                                                
+#                                     ....cc..........ccccccuooc......ccc.ccccc.ccccc.                                               
+#                                     .zoouu.................ccocc....ccccccccccccc.co.                                              
+#                   ..........c.......cc..ccc...............cc.ccoc...ccccccc.ccccc.ccc.                                             
+#         ..cccccccccccccccccccccc...cuo...c.................c...cccc.cccccccccccccc..oc                                             
+#   ..ccocc.....ccccc..............cozuc..c.....c.c.ccc.c...ccccc.ccccccccccccccccc.c.cc.                                            
+# czc..        ..................cuhc.....  .cccccc......ccccccccccccccccccc.ccccccccc.cc.                                           
+# .cc.          ....c.........ccuzc.        ..c....      .........ccccccccccccccc.ccc..cu.                                           
+#  .cc........    .c.........couc                                  ..ccccccccccccc.c.cccc..                                          
+#   cc..........   .........czc.                                      .cccccccccccccccc..cc                                          
+#    cc........... .......cuz.                                          .ccccccc.cccccc..oc                                          
+#    .cc. ........ccccc..czu.                                             .cccccccc.ccc..cc.   ..                                    
+#    .cc.....ccccccccccczc.                   ...                          .ccccccccccc.c..  cccc.                                  
+#       czccccccccccc.ccuo.          ..     ....                             .ccccccccccc..c..c. .cc.                                
+#        czc.ccccccc.cczc.           ...  ....                                 cccccccccc..cuc..   cc.                               
+#         cuoc.cccccccou.             ......                                    ccc.ccccc.cuc....   .cc                              
+#          .cucccc..cuz.               ...                                      .ccccc.cc.cu. ...    .co.                            
+#            cucc.cccuu.                .                                        .ccccc...co. ...   .  .c.                           
+#             .cucccoo.                                      ..c.                 cccc.cccco.....       .cc.                         
+#               .czuhc                                    .chhhhc                 .ccccccccc.....      ...cc.                        
+#                 .uhc                                  .uhhhhc  ...              .ccccc..cc.....     .cc..cc.                       
+#                   c.                                  chhhhzc   ohc             ..ccccc.cc. ...    .ccc...cc.                      
+#                  ... .cc..                             chhzhhzcohhho             .cccc..cuc....   .ccccc...cc.                     
+#                 .c. .chhhhhc.                         .chzzzhhhhhhhhc            .ccccc.couc..   .ccccccc...oc                     
+#                 .u. ...cchhhhu.         ..cc..c.       chhzzuzzzuzzhh.          ..ccccc..uoccc...ccccccccc...cc                    
+#                .c.  ...   ..cc.    ..cccoouoccuuo.     .hhzuzzzzzzzhh.          .ccccc..cooccoccccccc.cccc....oc.                  
+#                cc    ..            .cuoocccccccoouc     chhhzzzzzzhhz.          .cccccc.cucccccccccccc.ccccc....co..               
+#                cc                 .coccccccccccccuu.     chhhhhhhhhzc....      .ccccccccccccccccc.ccccccc.ccccc...ccc.             
+#                cc                  ccccccccccccocooc.     .ohhhhhzc.....       .ccccc..coccccccccc.ccccccccccccccc..ccc.           
+#                cc                  cccccccccccccc.cc.        ..........       ..ccccc..cucccccccccccccc.ccccc.ccccccc.coc          
+#                .c.                 .cucccccc.......c.              .          .cccc..ccocccccccccccccccccc.ccccc.cccccozc.         
+#                 .o.                 czocc..........c.                        .ccccc..cucccccccc.ccccccccccccc.cccccccoo.           
+#                  .cc.               .ccc........ccc.                        .cccc...cucccccccccc.ccccccccccccccccccoc.             
+#                    cuc.               ..ccccc.ccc.                  .cuoc. .cccc..czucccccccccccc.ccccccccccccccccc.               
+#                     .cc..                ......      ..c.         .coocccucccc..cozzoccccccccccc.ccc.ccc.ccccccc.                  
+#                      .ccc.                        .cc..z.      .cuc....czucc.ccc.  cuocccccc.ccc.ccccc.ccccc.                     
+#                         .ccc.                    .c.  .c.   .cooc......coccccc.     .zucccccc.cccccc..ccc.                        
+#                            .zhuc.                .c.    c...coc.........ozucc.        cuoc..cccccccc.ccc.                          
+#                             .hhzuzucc.           ..     .cucc..........coc.            coccccccccc.ccc.                            
+#                              .cuuc.  .ccc...    ..      coc...........cc.               .uccc.ccccoc.                              
+#            .cuuccc..         .ccuc.      ....  ..     .cc............cu.                .coc.cccc.                                
+#            .ccc.cccccouocc..   .cuzhuc.               ..cc..........ccc.                  .cccc.                                   
+#           .cc............cccoccccc.cc.               .ccc.........c.cuc                   cococ                                    
+#          .cc.......................              ...cccc.......cccccc.                   cocc.                                    
+#            .cc...................c.           ....cccc.ccccc..cccc.cc.                   .coc.                                     
+#             cuc................cc.        ...cccccc...cccccccccc..cc.          ..       .ccco.  .c.   ...                          
+#             .ccc...............c.     ...ccccc........cccccccc...cc.   ..c.   .ccc. ... .oc.   cc.     .....                       
+#             .coc.............c.   .ccccc.............ccccccc..cc.       .. .....   cc...cc...c.   ...   .cc.                      
+#              coccc..............cccc...............ccc.ccc..czc          .cc.    .c. .cccc .    .c...                            
+#                 .cucccc.........c...................ccc.ccccc.coc     ..c.      ....   .uc.     .cc   ...                          
+#                   .cccccc...........................cccccccccccc.       ..    ...... ..coc     ...      ......                     
+#                     ..ouc..........................ccccc.cc..cu.          ....c...  .chhu.  .............cc.c.                     
+#                       .ccccc.......ccc.............ccccccccccc.         .c.. ...    .ozc.   . .cc..........                        
+#                         cuccccccccc...............ccccccc.ccuc          cc.   .     .cz.      ...       ...                        
+#                         .coc.cccc................cccccccccccc          .cc   ....ccccc.       .c..      .c.                        
+#                           ccocc.................ccccccc.cccc.         ....   ..cczhoc..       .c..  .  ..c.                        
+#                            cuc...................ccccc.cco.                  ..czuc.                                               
+#                           cc...................ccccc..ccc.                 .cczzc.                                                 
+#                         .cc...................cccc...cc.               ..cczhzc.                                                   
+#                       .cc....................cc...cczzuc...........ccccchhhc..                                                     
+#                       .hc..................ccccouzhzoooocccccccccouzhhzo..                                                         
+#                       .cucc...........ccccccccc.    ....ccccococcc..                                                               
+#                         .ooccccccccccccccoc.                                                                                       
+#                          .ccccccccccc..                                                                                           
+                                                                                                                                                      
 # echo massage
 echo 'Auto-Compile By Liu Tianyou'
 
